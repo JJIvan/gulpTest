39f001c4f6d527ba423dc9ad4c824e6f980573c0
ivan <ivan555ivan555@gmail.com>
2018-03-13 00:28:02 +0800

    addgulpsass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
 gulp.task('concat', function() {
   return gulp.src('./src/*.js')
     .pipe(concat('all.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./dist/'));
 });
+gulp.task('sass', function () {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/stylesheet'));
+});
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/**/*.scss', ['sass']);
+});
+gulp.task('default',['concat','sass','sass:watch'])
